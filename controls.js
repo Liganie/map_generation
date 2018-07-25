d0792349cded40ad0d2120e252028f7ad5705745
pjacolot <pierre.jacolot@gmail.com>
2018-07-25 16:09:29 +0200

    Corrected the renderer to only redraw what is needed

--- controls.js
+++ controls.js
@@ -85,35 +85,69 @@ var TerrainSVG = TerrainDiv.insert("svg")
 // The different rendering terrainOptions
 var terrainOptions = {
     mapViewer: true,
-    cities: true
+    cities: true,
+    drawTrigger: 'None'
 };
 
+function TerrainDrawColoring() {
+    terrainOptions.drawTrigger = 'Coloring';
+    TerrainDraw();
+}
+
 function TerrainDraw() {
-    if (terrainRender==null) terrainRender = doTerrain(TerrainSVG, TerrainParams);
+    var selected_view = d3.select('select').property('value'); // background coloring
+
+    if (terrainRender==null) { // Special case for the very first map creation
+        terrainRender = doTerrain(TerrainSVG, TerrainParams);
+        terrainOptions.drawTrigger = 'None';
+    }
 
     // clear renderer
-    TerrainSVG.selectAll('path').remove();
-    TerrainSVG.selectAll('line.slope').remove();
-    TerrainSVG.selectAll('circle.city').remove();
-    TerrainSVG.selectAll('text').remove();
+    if( (terrainOptions.drawTrigger == 'Coloring' && selected_view != 'No coloring') || terrainOptions.drawTrigger == 'None') {// If we change the background, all the rest need to be put again on the foreground
+        TerrainSVG.selectAll('path').remove();
+        TerrainSVG.selectAll('line.slope').remove();
+        TerrainSVG.selectAll('circle.city').remove();
+        TerrainSVG.selectAll('text').remove();
+    } else if (terrainOptions.drawTrigger == 'Coloring'){
+        TerrainSVG.selectAll('path.field').remove();
+        TerrainSVG.selectAll('path.field_shading').remove();
+    }
+    if(terrainOptions.drawTrigger == 'Terrain') {// We need to redraw the terrain, background
+        TerrainSVG.selectAll('path.river').remove();
+        TerrainSVG.selectAll('path.river_background').remove();
+        TerrainSVG.selectAll('path.coast').remove();
+        TerrainSVG.selectAll('line.slope').remove();
+        if(terrainOptions.mapViewer) { // If we are drawing the terrain then we need to udpate the cities to be on the foreground
+            TerrainSVG.selectAll('path.border').remove();
+            TerrainSVG.selectAll('circle.city').remove();
+            TerrainSVG.selectAll('text').remove();
+        }
+    }
+    if(terrainOptions.drawTrigger == 'Cities') {// We need to redraw the cities only
+        TerrainSVG.selectAll('path.border').remove();
+        TerrainSVG.selectAll('circle.city').remove();
+        TerrainSVG.selectAll('text').remove();
+    }
 
     // render
-    var selected_view = d3.select('select').property('value');
-    if      (selected_view == 'Heightmap')  {visualizeVoronoi(TerrainSVG, terrainRender.h, 0);}
-    else if (selected_view == 'Slope')  {visualizeVoronoi(TerrainSVG, terrainRender.slope, 0, 10);}
-    else if (selected_view == 'Erosion')    {visualizeVoronoi(TerrainSVG, erosionRate(terrainRender.h));}
-    else if (selected_view == 'City Score') {visualizeVoronoi(TerrainSVG, terrainRender.score, d3.max(terrainRender.score) - 0.5);}
-    else if (selected_view == 'Regions')    {visualizeVoronoi(TerrainSVG, terrainRender.terr);}
-    else if (selected_view == 'Coloring')   {visualizeTerrain(TerrainSVG, terrainRender);}
+    if(terrainOptions.drawTrigger == 'Coloring' || terrainOptions.drawTrigger == 'None') {
+        if      (selected_view == 'Heightmap')  {visualizeVoronoi(TerrainSVG, terrainRender.h, 0);}
+        else if (selected_view == 'Slope')  {visualizeVoronoi(TerrainSVG, terrainRender.slope, 0, 10);}
+        else if (selected_view == 'Erosion')    {visualizeVoronoi(TerrainSVG, erosionRate(terrainRender.h));}
+        else if (selected_view == 'City Score') {visualizeVoronoi(TerrainSVG, terrainRender.score, d3.max(terrainRender.score) - 0.5);}
+        else if (selected_view == 'Regions')    {visualizeVoronoi(TerrainSVG, terrainRender.terr);}
+        else if (selected_view == 'Coloring')   {visualizeTerrain(TerrainSVG, terrainRender);}
+    }
 
-    if (terrainOptions.mapViewer) {
+    if (terrainOptions.mapViewer && terrainOptions.drawTrigger != 'Cities') {
         drawPaths(TerrainSVG, 'coast', terrainRender.coasts);
         visualizeSlopes(TerrainSVG, terrainRender);
-        if (selected_view == 'Coloring') drawPaths(TerrainSVG, 'riverBackground',  terrainRender.rivers, '#000000', 3);
+        if (selected_view == 'Coloring') drawPaths(TerrainSVG, 'river_background',  terrainRender.rivers, '#000000', 3);
         drawPaths(TerrainSVG, 'river', terrainRender.rivers, selected_view == 'Coloring' ? '#00b6dd': '#000000');
     }
 
-    if (terrainOptions.cities) {
+    if (terrainOptions.cities && 
+           !( (!terrainOptions.mapViewer && terrainOptions.drawTrigger == 'Terrain') || (terrainOptions.drawTrigger == 'Coloring' && selected_view == 'No coloring') ) ) {
         drawPaths(TerrainSVG, 'border', terrainRender.borders);
         visualizeCities(TerrainSVG, terrainRender);
         drawLabels(TerrainSVG, terrainRender);
@@ -121,7 +155,7 @@ function TerrainDraw() {
 }
 
 var ViewCombobox = TerrainDiv.append("select")
-.on('change',TerrainDraw)
+.on('change',TerrainDrawColoring)
 .selectAll('option')
     .data(["No coloring", "Heightmap", "Slope", "Erosion", "Regions", "City Score", "Coloring"]).enter()
     .append('option')
@@ -131,6 +165,7 @@ TerrainDiv.append("button")
     .text("Generate new Map")
     .on("click", function () {
         terrainRender = doTerrain(TerrainSVG, TerrainParams);
+        terrainOptions.drawTrigger = 'None';
         TerrainDraw();
     });
 
@@ -139,6 +174,7 @@ var TerrainBut = TerrainDiv.append("button")
     .on("click", function () {
         terrainOptions.mapViewer = !terrainOptions.mapViewer;
         TerrainBut.text(terrainOptions.mapViewer ? "Hide Terrain" : "Show Terrain");
+        terrainOptions.drawTrigger = 'Terrain';
         TerrainDraw();
     });
 
@@ -147,6 +183,7 @@ var CitiesBut = TerrainDiv.append("button")
     .on("click", function () {
         terrainOptions.cities = !terrainOptions.cities;
         CitiesBut.text(terrainOptions.cities ? "Hide Cities" : "Show Cities");
+        terrainOptions.drawTrigger = 'Cities';
         TerrainDraw();
     });
 
